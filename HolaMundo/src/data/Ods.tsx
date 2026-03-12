0b0f6f847f36e952d5dd31fb449d7f5b5780e122
DeityLimon <limoncete0@gmail.com>
2026-03-12 12:15:14 +0100

    añadir lista de los 17 ODS al módulo Ods.tsx

--- HolaMundo/src/data/Ods.tsx
+++ HolaMundo/src/data/Ods.tsx
@@ -8,28 +8,88 @@ type EstadoODS = 'Aprobado' | 'Ejecucion' | 'Completado' | 'Rechazado';
 
     export const odsAlumnos:InterfazSostenible[] = [
         {
-            id:1,
-            odsFav:"ODS 1",
-            estado:'Rechazado'
+            id: 1,
+            odsFav: "Fin de la pobreza",
+            estado: 'En Ejecucion'
         },
         {
-            id:2,
-            odsFav:"ODS 2",
-            estado:'Ejecucion'
+            id: 2,
+            odsFav: "Hambre cero",
+            estado: 'En Ejecucion'
         },
         {
-            id:3,
-            odsFav:"ODS 3",
-            estado:'Ejecucion'
+            id: 3,
+            odsFav: "Salud y bienestar",
+            estado: 'Aprobado'
         },
         {
-            id:4,
-            odsFav:"ODS 4",
-            estado:'Ejecucion'
+            id: 4,
+            odsFav: "Educación de calidad",
+            estado: 'Aprobado'
         },
         {
-            id:5,
-            odsFav:"ODS 5",
-            estado:'Aprobado'
+            id: 5,
+            odsFav: "Igualdad de género",
+            estado: 'En Ejecucion'
+        },
+        {
+            id: 6,
+            odsFav: "Agua limpia y saneamiento",
+            estado: 'En Ejecucion'
+        },
+        {
+            id: 7,
+            odsFav: "Energía asequible y no contaminante",
+            estado: 'Completado'
+        },
+        {
+            id: 8,
+            odsFav: "Trabajo decente y crecimiento económico",
+            estado: 'En Ejecucion'
+        },
+        {
+            id: 9,
+            odsFav: "Industria, innovación e infraestructura",
+            estado: 'Aprobado'
+        },
+        {
+            id: 10,
+            odsFav: "Reducción de las desigualdades",
+            estado: 'Rechazado'
+        },
+        {
+            id: 11,
+            odsFav: "Ciudades y comunidades sostenibles",
+            estado: 'En Ejecucion'
+        },
+        {
+            id: 12,
+            odsFav: "Producción y consumo responsables",
+            estado: 'En Ejecucion'
+        },
+        {
+            id: 13,
+            odsFav: "Acción por el clima",
+            estado: 'Rechazado'
+        },
+        {
+            id: 14,
+            odsFav: "Vida submarina",
+            estado: 'En Ejecucion'
+        },
+        {
+            id: 15,
+            odsFav: "Vida de ecosistemas terrestres",
+            estado: 'En Ejecucion'
+        },
+        {
+            id: 16,
+            odsFav: "Paz, justicia e instituciones sólidas",
+            estado: 'Aprobado'
+        },
+        {
+            id: 17,
+            odsFav: "Alianzas para lograr los objetivos",
+            estado: 'En Ejecucion'
         }
     ];
